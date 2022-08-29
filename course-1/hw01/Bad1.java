251aef9f74cf9db02fb1f1a65dd9529e45fb710d
Cooper Simpson <cooper@coopersimpson.com>
2022-08-30 09:54:03 +1000

    Initial commit

---
+++ course-1/hw01/Bad1.java
@@ -0,0 +1,6 @@
+public class Bad1 {
+    public static void main(String[] args) {
+        String str = new String("01234567891");
+        System.out.println(str.length() - 5 + " is " + str.length() + " - 5");
+    }
+}
